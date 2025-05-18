1b01646b04f58c887de701b6f8ef05db0f388694
Nimira43 <n.rabone72@aol.com>
2025-05-18 10:11:50 +0100

    Corrected logic inside if statement

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ const isPublicRoute = createRouteMatcher([
 ])
 
 export default clerkMiddleware((auth, req) => {
-  if (isPublicRoute(req)) {
+  if (!isPublicRoute(req)) {
     auth().protect()
   }
 })
