b3f56ecdeb3bb8d39e3efd5847d3f511797b741b
Nimira43 <n.rabone72@aol.com>
2025-05-18 07:49:12 +0100

    HomePage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={cn(
-        'min-h-screen, bg-background, antialiased',
+        'min-h-screen',
         poppins.className
       )}>
         {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 export default function Sirius() {
   return (
-    <div>Sirius</div>
+    <div className='text-center container my-4 mx-auto'>
+      <h1 className='text-5xl mb-4 text-main logo'>Sirius</h1>
+      <div className='flex gap-2 justify-center'>
+        <Button className='uppercase'>Login</Button>
+        <Button className='uppercase'>Register </Button>
+      </div>
+    </div>
   )
 }
