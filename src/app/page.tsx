d4108444bb2cfcbccf1bc98577d094033eaacc28
Nimira43 <n.rabone72@aol.com>
2025-05-18 08:16:05 +0100

    Using Clerk's sign in and sign out buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
+import { SignInButton, SignUpButton } from '@clerk/nextjs'
 
 export default function Sirius() {
   return (
     <div className='text-center container my-4 mx-auto'>
       <h1 className='text-5xl mb-4 text-main logo'>Sirius</h1>
       <div className='flex gap-2 justify-center'>
-        <Button className='uppercase'>Login</Button>
-        <Button className='uppercase'>Register </Button>
+        <Button className='uppercase' asChild>
+          <SignInButton />
+        </Button>
+        <Button className='uppercase' asChild>
+          <SignUpButton />
+        </Button>
       </div>
     </div>
   )
