072a626d565f4c74f7aa627b880962f5eefaa5e1
Nimira43 <n.rabone72@aol.com>
2025-05-18 08:11:58 +0100

    Added ClerkProvider to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
+import { ClerkProvider } from '@clerk/nextjs'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -19,13 +20,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={cn(
-        'min-h-screen',
-        poppins.className
-      )}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={cn(
+          'min-h-screen',
+          poppins.className
+        )}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
