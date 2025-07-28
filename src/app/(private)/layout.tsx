91ea4c939e3eb424a8f2d22b239b089409d070a1
Nimira43 <n.rabone72@aol.com>
2025-07-28 07:12:47 +0100

    Creating private/layout.tsx

---
+++ src/app/(private)/layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react'
+
+export default function PrivateLayout({ children }: { children: ReactNode} ) {
+  return (
+    <>
+      <header></header>
+      <main className='container my-6 mx-auto'>
+        {children} 
+      </main>
+    </>
+  )
+}
