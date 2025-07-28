3d7965f3ca3d015f77269945f52b4dca45295121
Nimira43 <n.rabone72@aol.com>
2025-07-28 07:43:13 +0100

    Working on navbar logo

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,10 +1,21 @@
 import { ReactNode } from 'react'
+import { GiPolarStar } from 'react-icons/gi'
 
 export default function PrivateLayout({ children }: { children: ReactNode} ) {
   return (
     <>
-      <header></header>
-      <main className='container my-6 mx-auto'>
+      <header className='flex py-2 border-b bg-card'>
+        <nav className='font-medium flex items-center gap-6 container'>
+          <div className='flex items-center gap-2 font-medium mr-auto'>
+            <GiPolarStar className='size-6 text-main'/>
+            <span className='logo text-main'>Sirius</span>
+          </div>
+          <div className='ml-auto size-10'>
+
+          </div>
+        </nav>
+      </header>
+      <main className='container my-6'>
         {children} 
       </main>
     </>
