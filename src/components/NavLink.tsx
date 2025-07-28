9e3c0942769c1d4a0fe472acadafb72e6b3413dd
Nimira43 <n.rabone72@aol.com>
2025-07-28 07:55:38 +0100

    Imports for NavLink

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,3 +1,10 @@
+'use client'
+
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import { useParams } from 'next/navigation'
+import { ComponentProps } from 'react'
+
 export default function NavLink() {
   return (
     <div>NavLink</div>
