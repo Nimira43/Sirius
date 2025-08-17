a45c1b97d1d31bedd896072921e6b5605b12ea76
Nimira43 <n.rabone72@aol.com>
2025-08-17 12:46:09 +0100

    EventsPage - 9

--- src/app/(private)/events/page.tsx
+++ src/app/(private)/events/page.tsx
@@ -17,7 +17,7 @@ export default async function EventsPage() {
   return (
     <>
       <div className='flex gap-4 items-baseline'>
-        <h1 className='text-3xl lg:text-4xl xl:text-5xl font-medium text-main mb-6'>Events</h1>
+        <h1 className='text-3xl lg:text-4xl xl:text-5xl font-medium text-dark mb-6'>Events</h1>
         <Button asChild>
           <Link 
             className='uppercase'
@@ -31,8 +31,22 @@ export default async function EventsPage() {
       {events.length > 0 
         ? (<h1>Events</h1>) 
         : (
-          <div>
-            <BsCalendar3 />
+          <div className='flex flex-col items-center gap-4'>
+            <BsCalendar3 className='size-16 mx-auto' />
+            <span>There are currently no events to show. Create your first event to get started.</span>
+            <Button 
+              size='lg'
+              className='text-lg'
+              asChild
+            >
+              <Link 
+                className='uppercase'
+                href='/events/new'
+              >
+                <BsCalendarCheck className='mr-2 size-6' />
+                New Event
+              </Link>
+            </Button>
           </div>
         )
       }
