dde4582357b3120548ae8d0e6309f8cb7b326467
Nimira43 <n.rabone72@aol.com>
2025-09-09 01:22:36 +0100

    createEvent - 3

--- src/server/actions/events.ts
+++ src/server/actions/events.ts
@@ -1,15 +1,19 @@
 'use server'
 
+import { db } from '@/drizzle/db'
+import { EventTable } from '@/drizzle/schema'
 import { eventFormSchema } from '@/schema/events'
+import { auth } from '@clerk/nextjs/server'
 import 'use-server'
 import { z } from 'zod'
 
 export async function createEvent(unsafeData: z.infer<typeof eventFormSchema>) {
+  const { userId } = auth()  
   const { success, data} = eventFormSchema.safeParse(unsafeData)
 
   if (!success) {
     return { error: true}
   }
 
-  
+  db.insert(EventTable).values({...data, clerkUserId: userId})
 }
