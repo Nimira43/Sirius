6321a1b7eeff2ad589d2c2a8b8d1be21cdf59a79
Nimira43 <n.rabone72@aol.com>
2025-09-09 02:07:58 +0100

    formatEventDescription - 2

--- src/lib/formatters.ts
+++ src/lib/formatters.ts
@@ -1,3 +1,7 @@
 export function formatEventDescription(durationInMinutes: number) {
+  const hours = Math.floor(durationInMinutes / 60)
+  const minutes = durationInMinutes % 60
+  const minutesString = `${minutes} ${minutes > 1 ? 'mins' : 'min'}`
+  const hoursString = `${hours} ${hours > 1 ? 'hrs' : 'hr'}`
   
 }
