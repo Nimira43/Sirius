0f1372a94288dea6bcefa9ac038fdce0e9cb91dd
Nimira43 <n.rabone72@aol.com>
2025-10-01 13:58:57 +0100

    userId and redirectToSignIn from auth

--- src/app/(private)/events/[eventId]/edit/page.tsx
+++ src/app/(private)/events/[eventId]/edit/page.tsx
@@ -1,7 +1,10 @@
 import { EventForm } from '@/components/forms/EventForm'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { auth } from '@clerk/nextjs/server'
 
 export default function EditEventPage() {
+  const { userId, redirectToSignIn } = auth()
+
   return (
     <Card className='max-w-md mx-auto'>
       <CardHeader>
