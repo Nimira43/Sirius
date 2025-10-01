152ab958cc196aa9c22baf182184a8c9615bd930
Nimira43 <n.rabone72@aol.com>
2025-10-01 14:13:10 +0100

    Params, type and query in EditEventPage.

--- src/app/(private)/events/[eventId]/edit/page.tsx
+++ src/app/(private)/events/[eventId]/edit/page.tsx
@@ -1,11 +1,21 @@
 import { EventForm } from '@/components/forms/EventForm'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { db } from '@/drizzle/db'
 import { auth } from '@clerk/nextjs/server'
 
-export default function EditEventPage() {
+export default function EditEventPage({params: {
+  eventId
+}}: {
+  params: { eventId: string}
+}) {
   const { userId, redirectToSignIn } = auth()
 
-  if (userId == null) redirectToSignIn()
+  if (userId == null) return redirectToSignIn()
+
+  const event = await db.query.EventTable.findFirst({
+    where: ({ id, clerkUserId }, { and, eq }) => 
+      and(eq(clerkUserId, userId), eq(id, eventId)),
+  })
 
   return (
     <Card className='max-w-md mx-auto'>
