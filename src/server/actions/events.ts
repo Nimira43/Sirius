6b29e69b2b2cad6c370874fecdb60c006a2d8a13
Nimira43 <n.rabone72@aol.com>
2025-10-01 17:45:31 +0100

    Working on deleteEvent function

--- src/server/actions/events.ts
+++ src/server/actions/events.ts
@@ -49,3 +49,29 @@ export async function updateEvent(
 
   redirect('/events')
 }
+
+export async function deleteEvent(
+  id: string,
+  unsafeData: z.infer<typeof eventFormSchema>
+): Promise<{ error: boolean | undefined }> {
+  const { userId } = auth()  
+  const { success, data} = eventFormSchema.safeParse(unsafeData)
+
+  if (!success || userId == null) {
+    return { error: true}
+  }
+
+  const { rowCount } = await db
+    .update(EventTable)
+    .set({...data})
+    .where(
+      and(
+        eq(EventTable.id, id), 
+        eq(EventTable.clerkUserId, userId)))
+
+  if (rowCount === 0) {
+    return { error: true}
+  }
+
+  redirect('/events')
+}
