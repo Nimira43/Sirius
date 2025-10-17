e0b1de7d0f3007d024d9dc6fb4b434f37d5de2b8
Nimira43 <n.rabone72@aol.com>
2025-10-17 01:58:38 +0100

    schedule - 1

--- src/app/(private)/schedule/page.tsx
+++ src/app/(private)/schedule/page.tsx
@@ -1,11 +1,16 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { db } from '@/drizzle/db'
 import { auth } from '@clerk/nextjs/server'
 
-export default function SchedulePage() {
+export default async function SchedulePage() {
   const { userId, redirectToSignIn } = auth()
   
-  if (!userId == null) return redirectToSignIn
+  if (!userId == null) return redirectToSignIn()
   
+  const schedule = await db.query.ScheduleTable.findFirst({
+    where: (({ clerkUserId }, { eq }) => eq(clerkUserId, userId))
+  })
+
   return (
     <Card className='max-w-md mx-auto'>
       <CardHeader>
