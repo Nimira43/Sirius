db5b14978571120d1bcc4dea33c550bc5e790384
Nimira43 <n.rabone72@aol.com>
2025-10-17 11:36:31 +0100

    saveSchedule function - 2

--- src/server/actions/schedule.ts
+++ src/server/actions/schedule.ts
@@ -1,5 +1,9 @@
 'use server'
 
 import 'use-server'
+import { scheduleFormSchema } from '@/schema/schedule'
+import { z } from 'zod'
 
-export async function saveSchedule() {}
+export async function saveSchedule(values: z.infer<typeof scheduleFormSchema>) {
+
+}
