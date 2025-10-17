000cbc0278828ebe9119695645b0ec3f91ef3c62
Nimira43 <n.rabone72@aol.com>
2025-10-17 06:02:14 +0100

    scheduleFormSchema - 2

--- src/schema/schedule.ts
+++ src/schema/schedule.ts
@@ -1,6 +1,10 @@
+import { DAYS_OF_WEEK_IN_ORDER } from '@/data/constants'
 import { z } from 'zod'
 
 export const scheduleFormSchema = z.object({
-
+  timezone: z.string().min(1, 'Required'),
+  availabilities: z.array(z.object({
+    dayOfWeek: z.enum(DAYS_OF_WEEK_IN_ORDER)
+  }))
 
 })
