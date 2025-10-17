8329b62f60f1e0e87165b8f8ced6a8c4812df048
Nimira43 <n.rabone72@aol.com>
2025-10-17 11:43:04 +0100

    saveSchedule - 4

--- src/server/actions/schedule.ts
+++ src/server/actions/schedule.ts
@@ -4,12 +4,23 @@ import 'use-server'
 import { scheduleFormSchema } from '@/schema/schedule'
 import { z } from 'zod'
 import { auth } from '@clerk/nextjs/server'
+import { db } from '@/drizzle/db'
+import { ScheduleTable } from '@/drizzle/schema'
 
-export async function saveSchedule(unsafeData: z.infer<typeof scheduleFormSchema>) {
+export async function saveSchedule(
+  unsafeData: z.infer<typeof scheduleFormSchema>
+) {
   const { userId } = auth()  
   const { success, data} = scheduleFormSchema.safeParse(unsafeData)
 
   if (!success || userId == null) {
     return { error: true}
   }
+
+  const { availabilities, ...scheduleData } = data
+
+  await db.insert(ScheduleTable).values({
+    ...scheduleData,
+    clerkUserId: userId
+  })
 }
