ed71630eb74caf86012c98d02cdaf2a482aa9d52
Nimira43 <n.rabone72@aol.com>
2025-10-17 01:50:58 +0100

    Destructure userId and redirectToSignIn - auth

--- src/app/(private)/schedule/page.tsx
+++ src/app/(private)/schedule/page.tsx
@@ -1,14 +1,17 @@
-import { EventForm } from '@/components/forms/EventForm'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { auth } from '@clerk/nextjs/server'
 
-export default function NewEventPage() {
+export default function SchedulePage() {
+  const { userId, redirectToSignIn } = auth()
+  
+  
   return (
     <Card className='max-w-md mx-auto'>
       <CardHeader>
-        <CardTitle>New Event</CardTitle>
+        <CardTitle>Schedule</CardTitle>
       </CardHeader>
       <CardContent>
-        <EventForm />
+        <h1>Under Contruction</h1>
       </CardContent>     
     </Card>
   )
