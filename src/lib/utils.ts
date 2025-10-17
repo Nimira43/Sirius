37253f8f72c1853966c8cd7f71744545e8380094
Nimira43 <n.rabone72@aol.com>
2025-10-17 06:45:33 +0100

    Moved timeToInt into utils. Also created an alternative version timeToInt2

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function timeToInt(time: string) {
+  return parseFloat(time.replace(':', '.'))
+}
+
+export function timeToInt2(time: string) {
+  const [hours, minutes] = time.split(':').map(Number)
+  return hours * 60 + minutes
+}
