cd641234189748b4cad4493748c29be15e538cfd
Nimira43 <n.rabone72@aol.com>
2025-10-17 06:20:54 +0100

    scheduleFormSchema - 4

--- src/schema/schedule.ts
+++ src/schema/schedule.ts
@@ -17,5 +17,11 @@ export const scheduleFormSchema = z.object({
           .string()
           .regex(/^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/, 'Time must be in the format HH:MM'),
       })
-    )
+    ).superRefine((availabilities, ctx) => {
+      availabilities.forEach((availibility, index) => {
+        const overlaps = availabilities.some((a, i) => {
+          
+        })
+      })
+    }),
 })
