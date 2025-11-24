d117cd5d6e7bba5d27311bc21177ffc9373837dd
Nimira43 <n.rabone72@aol.com>
2025-11-24 20:35:37 +0000

    Copying over EventCardProps and EventCard and adapting it for BookingPage

--- src/app/(public)/book/[clerkUserId]/page.tsx
+++ src/app/(public)/book/[clerkUserId]/page.tsx
@@ -1,6 +1,10 @@
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from '@/components/ui/card'
 import { db } from '@/drizzle/db'
+import { formatEventDescription } from '@/lib/formatters'
 import { clerkClient } from '@clerk/nextjs/server'
+import Link from 'next/link'
 import { notFound } from 'next/navigation'
+import { Button } from '@/components/ui/button' 
 
 export default async function BookingPage({
   params: { clerkUserId },
@@ -34,6 +38,49 @@ export default async function BookingPage({
       <div className='text-dark mb-6 max-w-sm mx-auto text-center'>
         Welcome to my Scheduling page. Please follow the instructions to add an event to my Calendar.
       </div>
+      <div className='grid gap-4 [grid-template-columns:repeat(auto-fill,minmax(300px,1fr))]'>
+        {events.map(event => (
+          <EventCard 
+            key={event.id} 
+            {...event}
+          />
+        ))}
+      </div>
     </div>
+  )
+}
+
+type EventCardProps = {
+  id: string
+  name: string
+  description: string | null
+  durationInMinutes: number
+}
+
+function EventCard({
+  id,
+  name,
+  description,
+  durationInMinutes,
+}: EventCardProps) {
+  return (
+    <Card className='flex flex-col border-main-light'>
+      <CardHeader>
+        <CardTitle>{name}</CardTitle>
+        <CardDescription>
+          {formatEventDescription(durationInMinutes)}
+        </CardDescription>
+      </CardHeader>
+      {description != null && (
+        <CardContent>
+          {description}
+        </CardContent>
+      )}
+      <CardFooter className='flex justify-end gap-2 mt-auto'>
+        <Button asChild>
+          <Link href={`/book/${clerkUserId}/edit`}>Edit</Link>
+        </Button>  
+      </CardFooter>   
+    </Card>
   )
 }
