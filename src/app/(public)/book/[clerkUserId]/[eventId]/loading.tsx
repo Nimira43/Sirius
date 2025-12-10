7eb233f4b5e8cdf1d83dd2925a64c182d1d9d753
Nimira43 <n.rabone72@aol.com>
2025-12-10 15:27:00 +0000

    Loading 2

--- src/app/(public)/book/[clerkUserId]/[eventId]/loading.tsx
+++ src/app/(public)/book/[clerkUserId]/[eventId]/loading.tsx
@@ -3,7 +3,8 @@ import { TbLoader } from 'react-icons/tb'
 export default function Loading() {
   return (
     <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-4 items-center'>
-      
+      <div className='text-3xl font-medium text-center text-main'>Loading...</div>
+      <TbLoader className='text-main size-24 animate-spin' />
     </div>
   )
 }
